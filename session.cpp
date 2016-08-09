290764f229feea2031a6fe243d5ed9a4b4546eca
Tom Joseph <tomjoseph@in.ibm.com>
2016-08-09 08:16:12 -0500

    Representation of an IPMI session
    IPMI session class encapsulates the details regarding an IPMI session.
    It includes the details of the remote session id, BMC session id
    Cipher suites, session state information.
    
    Change-Id: Iffae0e05f33d1b3aa32cc17e5a3c1368d53c1d38
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ session.cpp
@@ -0,0 +1,51 @@
+#include "session.hpp"
+
+#include <openssl/rand.h>
+
+#include <cstdlib>
+#include <ctime>
+
+#include "endian.hpp"
+
+namespace session
+{
+
+Session::Session(uint32_t inRemoteConsoleSessID, Privilege priv)
+{
+    remoteConsoleSessionID = endian::from_ipmi<uint32_t>(inRemoteConsoleSessID);
+    std::srand(std::time(0));
+    bmcSessionID = std::rand();
+    curPrivLevel = priv;
+    state = State::INACTIVE;
+}
+
+bool Session::isSessionActive()
+{
+    auto isActive = false;
+    auto currentTime = std::chrono::steady_clock::now();
+    auto elapsedSeconds = std::chrono::duration_cast<std::chrono::seconds>
+                          (currentTime - lastTime);
+
+    switch (state)
+    {
+        case State::SETUP_IN_PROGRESS:
+            if (elapsedSeconds < SESSION_SETUP_TIMEOUT)
+            {
+                isActive = true;
+            }
+            break;
+        case State::ACTIVE:
+            if (elapsedSeconds < SESSION_INACTIVITY_TIMEOUT)
+            {
+                isActive = true;
+            }
+            break;
+        default:
+            isActive = false;
+            break;
+    }
+
+    return isActive;
+}
+
+} // namespace session
