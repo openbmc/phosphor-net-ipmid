948e7eab4778e84e68312ae7dd7f41ae205fe7ec
Tom Joseph <tomjoseph@in.ibm.com>
2016-08-10 06:36:33 -0500

    IPMI Socket Class to encapsulate the socket operations
    This class represents the details regarding an IPMI client like IP
    address, port number and the file descriptor associated with it. The
    operations provided are reading the UDP packet, writing the packet.
    
    Change-Id: I58cde0edb04fd2fbedd3068de83379eebea8508d
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ socket_channel.cpp
@@ -0,0 +1,147 @@
+#include "socket_channel.hpp"
+
+#include <errno.h>
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <string>
+
+const char* SocketChannel::getRemoteAddress()
+{
+    char tmp[INET_ADDRSTRLEN] = { 0 };
+    inet_ntop(AF_INET, &address.inAddr.sin_addr, tmp, sizeof(tmp));
+    addressStr = tmp;
+    return addressStr.c_str();
+}
+
+int SocketChannel::read(std::vector<uint8_t>& outBuffer)
+{
+    auto rc = 0;
+    auto readSize = 0;
+    size_t readDataLen = 0;
+
+    if (ioctl(sockfd, FIONREAD, &readSize) < 0)
+    {
+        std::cerr << "E> SocketChannel::Read : ioctl failed with errno = " << errno;
+        return -errno;
+    }
+
+    outBuffer.resize(readSize);
+    auto bufferSize = outBuffer.size();
+    auto outputPtr = outBuffer.data();
+
+    address.addrSize = sizeof(address.inAddr);
+    readDataLen = recvfrom(sockfd,               // File Descriptor
+                           outputPtr ,           // Buffer
+                           bufferSize,           // Bytes requested
+                           0,                    // Flags
+                           &address.sockAddr,    // Address
+                           &address.addrSize);   // Address Length
+
+    if (readDataLen > 0) // Data read from the socket
+    {
+        std::cout << "I> SocketChannel::Read : DataIn Fd[" << sockfd << "] Req[" <<
+                  bufferSize << "] Recv[" << readDataLen << "]\n";
+    }
+    else if (readDataLen == 0) // Peer has performed an orderly shutdown
+    {
+        std::cerr << "E> SocketChannel::Read : Connection Closed Fd[" << sockfd <<
+                  "]\n";
+        outBuffer.resize(0);
+        rc = -1;
+    }
+    else if (readDataLen < 0) // Error
+    {
+        std::cerr << "E> SocketChannel::Read : Receive Error Fd[" << sockfd << "]" <<
+                  "errno = " << errno << "\n";
+        outBuffer.resize(0);
+        rc = -errno;
+    }
+
+    if (readDataLen < bufferSize)
+    {
+        std::cout << "I> SocketChannel::Read readDataLen less than requested : Req[" <<
+                  bufferSize << "] Recv[" << readDataLen << "]\n";
+    }
+
+    // Resize the vector to the actual data read from the socket
+    outBuffer.resize(readDataLen);
+    return rc;
+}
+
+int SocketChannel::write(std::vector<uint8_t>& inBuffer)
+{
+    auto rc = 0;
+    auto outputPtr = inBuffer.data();
+    auto bufferSize = inBuffer.size();
+    auto l_spuriousWakeup = false;
+    int writeDataLen = 0;
+
+    fd_set writeSet;
+    FD_ZERO(&writeSet);
+    FD_SET(sockfd, &writeSet);
+
+    struct timeval timeout;
+    timeout.tv_sec = SELECT_CALL_TIMEOUT;
+    timeout.tv_usec = 0;
+
+    do
+    {
+        l_spuriousWakeup = false;
+
+        rc = select((sockfd + 1), NULL, &writeSet, NULL, &timeout);
+
+        if (rc > 0)
+        {
+            if (FD_ISSET(sockfd, &writeSet))
+            {
+                address.addrSize = sizeof(address.inAddr);
+                writeDataLen = sendto(sockfd,                  // File Descriptor
+                                      outputPtr,               // Message
+                                      bufferSize,              // Length
+                                      MSG_NOSIGNAL,            // Flags
+                                      &address.sockAddr,       // Destination Address
+                                      address.addrSize);       // Address Length
+
+                if (writeDataLen == -1)
+                {
+                    std::cerr << "SocketChannel::Write: Write failed\n";
+                    inBuffer.resize(0);
+                    rc = -errno;
+                }
+                else if (static_cast<size_t>(writeDataLen) < bufferSize)
+                {
+                    std::cerr << "SocketChannel::Write: Complete data not written to the socket\n";
+                    inBuffer.resize(writeDataLen);
+                }
+            }
+            else
+            {
+                // Spurious wake up
+                std::cerr << "E> Spurious wake up on select (writeset)\n";
+                l_spuriousWakeup = true;
+            }
+        }
+        else
+        {
+            if (rc == 0)
+            {
+                // Timed out
+                std::cerr << "E> We timed out on select call (writeset)\n";
+            }
+            else
+            {
+                // Error
+                std::cerr << "E> select call (writeset) had an error : " << errno << "\n";
+                rc  = -errno;
+            }
+
+        }
+    }
+    while (l_spuriousWakeup);
+
+    return rc;
+}
---
+++ socket_channel.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <string>
+#include <vector>
+
+/** @class SocketChannel
+ *
+ *  @brief Provides encapsulation for UDP socket operations like Read, Peek,
+ *         Write, Remote peer's IP Address and Port.
+ */
+class SocketChannel
+{
+    public:
+
+        // Select call timeout is set arbitarily set to 30 sec
+        static constexpr size_t SELECT_CALL_TIMEOUT = 30;
+
+        struct SockAddr_t
+        {
+            union
+            {
+                sockaddr sockAddr;
+                sockaddr_in inAddr;
+            };
+            size_t addrSize;
+        };
+
+        /**
+         * @brief Constructor
+         *
+         * Initialize the IPMI socket object with the socket descriptor
+         *
+         * @param  i_fd
+         *    File Descriptor for the socket
+         *
+         * @return None
+         */
+        SocketChannel(int insockfd)
+        {
+            sockfd = insockfd;
+        }
+
+        /**
+         * @brief Fetch the IP address of the remote peer
+         *
+         * Returns the IP address of the remote peer which is connected to this socket
+         *
+         * @return Pointer to the address of the remote peer
+         */
+        const char* getRemoteAddress();
+
+        /**
+         * @brief Fetch the port number of the remote peer
+         *
+         * Returns the port number of the remote peer
+         *
+         * @return Port number
+         *
+         */
+        auto getPort()
+        {
+            return address.inAddr.sin_port;
+        }
+
+        /**
+         * @brief Read the incoming packet
+         *
+         * Reads the data available on the socket
+         *
+         * @param  outBuffer
+         *    In case of success, the vector is populated with the data available on the
+         *    socket and in case of error, the vector size is set to 0.
+         *
+         * @return Error upon read failure
+         */
+        int read(std::vector<uint8_t>& outBuffer);
+
+        /**
+         *  @brief Write the outgoing packet
+         *
+         *  Writes the data in the vector to the socket
+         *
+         *  @param  inBuffer
+         *      On input the size of the vector would be the number of bytes to write.
+         *      In case of success, the number of bytes successfully written to the
+         *      socket descriptor and in case of error, the size is set to 0.
+         *
+         *  @return Error upon write failure
+         */
+        int write(std::vector<uint8_t>& inBuffer);
+
+        /**
+         * @brief Returns file descriptor for the socket
+         */
+        auto getHandle(void) const
+        {
+            return sockfd;
+        };
+
+        ~SocketChannel() = default;
+        SocketChannel(const SocketChannel& right) = delete;
+        SocketChannel& operator=(const SocketChannel& right) = delete;
+        SocketChannel(SocketChannel&&) = delete;
+        SocketChannel& operator=(SocketChannel&&) = delete;
+
+    private:
+        int sockfd;
+        SockAddr_t address;
+        std::string addressStr;
+};
