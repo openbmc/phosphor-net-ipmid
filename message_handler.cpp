648293e8ba60d08954911e286797ecda9c51e9a1
Tom Joseph <tomjoseph@in.ibm.com>
2016-08-10 06:56:25 -0500

    Message Handler
    IPMI message Handler encapsulates the operations with respective to each
    IPMI message.The routines are there to read the message, execute the
    command and send the response.
    
    Change-Id: I607416f723510326748b3eba73f3a6557c40dd06
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>
    Signed-off-by: Tom <tomjoseph@in.ibm.com>

---
+++ message_handler.cpp
@@ -0,0 +1,196 @@
+#include "message_handler.hpp"
+
+#include <sys/socket.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "command_table.hpp"
+#include "message_parsers.hpp"
+#include "sessions_manager.hpp"
+#include "main.hpp"
+#include "message.hpp"
+
+namespace message
+{
+
+std::unique_ptr<Message> Handler::receive()
+{
+    std::vector<uint8_t> packet;
+    auto readStatus = 0;
+
+    // Read the packet
+    std::tie(readStatus, packet) = channel->read();
+
+    if (readStatus < 0)
+    {
+        std::cerr << "E> Error in Read : " << std::hex << readStatus << "\n";
+    }
+
+    // Unflatten the packet
+    auto message = Parser::unflatten(packet, sessionHeader);
+
+    if (message != nullptr)
+    {
+        auto session = std::get<session::Manager&>(singletonPool).getSession(
+                           message->bmcSessionID);
+        if (session == nullptr)
+        {
+            return nullptr;
+        }
+        else
+        {
+            sessionID = message->bmcSessionID;
+            message->rcSessionID = session->getRCSessionID();
+            session->updateLastTransactionTime();
+        }
+
+        return std::move(message);
+    }
+    else
+    {
+        return nullptr;
+    }
+    std::cout << "<<Handler::receive\n";
+}
+
+std::unique_ptr<Message> Handler::executeCommand(
+    Message* inMessage)
+{
+
+    // Get the CommandID to map into the command table
+    auto command = getCommand(inMessage);
+
+    auto inPayload(inMessage->payload);
+
+    // If the payload type is IPMI then remove the IPMI LAN Message header and
+    // trailer from the message payload
+    if (inMessage->payloadType == PayloadType::IPMI)
+    {
+        inMessage->payload.erase(inMessage->payload.begin(),
+                                 inMessage->payload.begin() +
+                                 sizeof(LAN::header::Request));
+        inMessage->payload.erase(inMessage->payload.end() - sizeof(
+                                     LAN::trailer::Response),
+                                 inMessage->payload.end());
+    }
+
+    auto output = std::get<command::Table&>(singletonPool).executeCommand(
+                      command,
+                      inMessage->payload,
+                      *this);
+
+    auto outMessage = std::make_unique<Message>();
+    outMessage->rcSessionID = inMessage->rcSessionID;
+
+    if (inMessage->payloadType == PayloadType::IPMI)
+    {
+        outMessage->payloadType = PayloadType::IPMI;
+
+        outMessage->payload.resize(sizeof(LAN::header::Response) +
+                                   output.size()
+                                   +
+                                   sizeof(LAN::trailer::Response));
+
+        auto reqHeader = reinterpret_cast<LAN::header::Request*>
+                         (inPayload.data());
+        auto respHeader = reinterpret_cast<LAN::header::Response*>
+                          (outMessage->payload.data());
+
+        // Add IPMI LAN Message Response Header
+        respHeader->rqaddr = reqHeader->rqaddr;
+        respHeader->netfn  = reqHeader->netfn | 0x04;
+        respHeader->cs     = ipmiCrc8bit(&(respHeader->rqaddr), 2);
+        respHeader->rsaddr = reqHeader->rsaddr;
+        respHeader->rqseq  = reqHeader->rqseq;
+        respHeader->cmd    = reqHeader->cmd;
+
+        auto assembledSize = sizeof(LAN::header::Response);
+
+        // Copy the output by the execution of the command
+        std::copy(output.begin(), output.end(),
+                  outMessage->payload.begin() + assembledSize);
+        assembledSize += output.size();
+
+        // Add the IPMI LAN Message Trailer
+        auto trailer = reinterpret_cast<LAN::trailer::Response*>
+                       (outMessage->payload.data() + assembledSize);
+        trailer->checksum = ipmiCrc8bit(&respHeader->rsaddr, assembledSize - 3);
+
+    }
+    else if (inMessage->payloadType == PayloadType::OPEN_SESSION_REQUEST)
+    {
+        outMessage->payloadType = PayloadType::OPEN_SESSION_RESPONSE;
+        outMessage->payload.resize(output.size());
+        std::copy(output.begin(), output.end(), outMessage->payload.begin());
+    }
+    else if (inMessage->payloadType == PayloadType::RAKP1)
+    {
+        outMessage->payloadType = PayloadType::RAKP2;
+        outMessage->payload.resize(output.size());
+        std::copy(output.begin(), output.end(), outMessage->payload.begin());
+    }
+    else if (inMessage->payloadType == PayloadType::RAKP3)
+    {
+        outMessage->payloadType = PayloadType::RAKP4;
+        outMessage->payload.resize(output.size());
+        std::copy(output.begin(), output.end(), outMessage->payload.begin());
+    }
+
+    return std::move(outMessage);
+}
+
+uint32_t Handler::getCommand(Message* message)
+{
+    uint32_t command = 0 ;
+
+    if (message)
+    {
+        command |= (0 << 24);
+        command |= (static_cast<uint8_t>(message->payloadType) << 16);
+        if (message->payloadType == PayloadType::IPMI)
+        {
+            command |= ((reinterpret_cast<LAN::header::Request*>
+                         (message->payload.data()))->netfn) << 8;
+            command |= (reinterpret_cast<LAN::header::Request*>
+                        (message->payload.data()))->cmd;
+        }
+    }
+
+    return command;
+}
+
+int Handler::send(Message* outMessage)
+{
+    // Flatten the packet
+    auto packet = Parser::flatten(outMessage, sessionHeader);
+
+    // Read the packet
+    auto writeStatus = channel->write(packet);
+    if (writeStatus < 0)
+    {
+        std::cerr << "E> Error in writing : " << std::hex << writeStatus
+                  << "\n";
+    }
+
+    return writeStatus;
+}
+
+uint8_t Handler::ipmiCrc8bit(const uint8_t* ptr, const int len)
+{
+    uint8_t csum = 0;
+    auto inc = 0;
+
+    while (inc != len)
+    {
+        csum += *(ptr + inc);
+        inc++;
+    }
+
+    return 0x100 - csum;
+}
+
+} //namespace message
+
---
+++ message_handler.hpp
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "message.hpp"
+#include "message_parsers.hpp"
+#include "session.hpp"
+
+namespace message
+{
+
+class Handler
+{
+    public:
+
+        Handler(std::shared_ptr<udpsocket::Channel> channel):
+            channel(channel) {}
+
+        Handler() = delete;
+        ~Handler() = default;
+        Handler(const Handler&) = delete;
+        Handler& operator=(const Handler&) = delete;
+        Handler(Handler&&) = delete;
+        Handler& operator=(Handler&&) = delete;
+
+        /*
+         * @brief Receive the IPMI packet
+         *
+         * Read the data on the socket, get the parser based on the Session
+         * header type and flatten the payload and generate the IPMI message
+         *
+         * @return IPMI Message on success and nullptr on failure
+         *
+         */
+        std::unique_ptr<Message> receive();
+
+        /*
+         * @brief Process the incoming IPMI message
+         *
+         * The incoming message payload is handled and the command handler for
+         * the Network function and Command is executed and the response message
+         * is returned
+         *
+         * @param [in] Incoming Message
+         *
+         * @return Outgoing message on success and nullptr on failure
+         */
+        std::unique_ptr<Message> executeCommand(Message* inMessage);
+
+        /*
+         * @brief Send the outgoing message
+         *
+         * The payload in the outgoing message is flattened and sent out on the
+         * socket
+         *
+         * @param [in] Outgoing Message
+         *
+         * @return Zero on success and non-zero on failure
+         */
+        int send(Message* outMessage);
+
+        auto getChannel()
+        {
+            return channel;
+        }
+
+        auto getSessionID()
+        {
+            return sessionID;
+        }
+
+    private:
+
+        // Socket channel for communicating with the remote client
+        std::shared_ptr<udpsocket::Channel> channel;
+
+        // BMC Session ID for the Channel
+        uint32_t sessionID;
+
+        // IPMI 1.5 or IPMI 2.0 Session Header
+        Parser::SessionHeader sessionHeader;
+
+        /*
+         * @brief Extract the command from the IPMI payload
+         */
+        uint32_t getCommand(Message* message);
+
+        /*
+         * @brief Calculate 8 bit 2's complement checksum
+         *
+         * Initialize checksum to 0. For each byte, checksum = (checksum + byte)
+         * modulo 256. Then checksum = - checksum. When the checksum and the
+         * bytes are added together, modulo 256, the result should be 0.
+         */
+
+        uint8_t ipmiCrc8bit(const uint8_t* ptr, const int len);
+};
+
+} //namespace message
+
+
+
