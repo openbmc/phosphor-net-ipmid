0d8c1262355c1e86ebe4e4ecc2a8b1275d943d73
Tom Joseph <tomjoseph@in.ibm.com>
2016-12-06 16:56:04 +0530

    API for reading System GUID
    Implementation for reading system GUID which would be consumed
    by RAKP12, RAKP34 and System GUID commands.
    
    Change-Id: Ibee1c398368f67055cdba2a0f4d16c981e38c035
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ command/guid.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstddef>
+
+#include "comm_module.hpp"
+
+namespace command
+{
+
+constexpr size_t BMC_GUID_LEN = 16;
+
+/*
+ * @brief Get System GUID
+ *
+ * @param[in] buffer - Buffer to be populated with the GUID
+ * @param[in] numBytes - The number of bytes allotted for populating GUID
+ *
+ * @return If UUID is successfully read from the Chassis DBUS object, then the
+ *         buffer is populated with GUID.
+ */
+void getSystemGUID(uint8_t* buffer, const size_t numBytes = BMC_GUID_LEN);
+
+} // namespace command
