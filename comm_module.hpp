01ac4843517dc771b417feae49d1a27754d7d07c
Tom Joseph <tomjoseph@in.ibm.com>
2016-12-06 16:52:34 +0530

    IPMI Communication module header
    RAKP return codes and session setup command declaration.
    
    Change-Id: I04d69651db6b32e9273e977b97ae664e6f002cc3
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ comm_module.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstdint>
+
+#include "message_handler.hpp"
+
+namespace command
+{
+
+/*
+ * @enum RAKP_ReturnCode
+ *
+ * RMCP+ and RAKP Message Status Codes
+ *
+ */
+enum class RAKP_ReturnCode : uint8_t
+{
+    NO_ERROR,
+    INSUFFICIENT_RESOURCE,
+    INVALID_SESSION_ID,
+    INVALID_PAYLOAD_TYPE,
+    INVALID_AUTH_ALGO,
+    INVALID_INTEGRITY_ALGO,
+    NO_MATCH_AUTH_PAYLOAD,
+    NO_MATCH_INTEGRITY_PAYLOAD,
+    INACTIVE_SESSIONID,
+    INACTIVE_ROLE,
+    UNAUTH_ROLE_PRIV,
+    INSUFFICIENT_RESOURCES_ROLE,
+    INVALID_NAME_LENGTH,
+    UNAUTH_NAME,
+    UNAUTH_GUID,
+    INVALID_INTEGRITY_CHECK_VALUE,
+    INVALID_CONF_ALGO,
+    NO_CIPHER_SUITE_MATCH,
+    ILLEGAL_PARAMETER,
+};
+
+/*
+ * @brief Register Session Setup commands to the Command Table
+ */
+void sessionSetupCommands();
+
+} // namespace command
