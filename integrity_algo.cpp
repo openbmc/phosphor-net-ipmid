684e77efd578e775b4038531a0880a35c12fcaef
Tom Joseph <tomjoseph@in.ibm.com>
2017-01-10 15:48:40 +0530

    Implementation of HMAC-SHA1-96 Integrity Algorithm
    Change-Id: Id301f0cb6e7cc9cde79385a18f3999d8d9c0feab
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- integrity_algo.cpp
+++ integrity_algo.cpp
@@ -23,6 +23,50 @@ Interface::Interface(const Buffer& sik, const Key& addKey, size_t authLength)
     authCodeLength = authLength;
 }
 
+bool AlgoSHA1::verifyIntegrityData(const Buffer& packet,
+                                   const size_t packetLen,
+                                   Buffer::const_iterator integrityData) const
+{
+    Buffer output(SHA_DIGEST_LENGTH);
+    unsigned int mdLen = 0;
+
+    if (HMAC(EVP_sha1(),
+             key1.data(), key1.size(),
+             packet.data() + message::parser::RMCP_SESSION_HEADER_SIZE,
+             packetLen - message::parser::RMCP_SESSION_HEADER_SIZE,
+             output.data(), &mdLen) == NULL)
+    {
+        throw std::runtime_error("Generating integrity data for verification "
+                                 "failed");
+    }
+
+    // HMAC generates Message Digest to the size of SHA_DIGEST_LENGTH, the
+    // AuthCode field length is based on the integrity algorithm. So we are
+    // interested only in the AuthCode field length of the generated Message
+    // digest.
+    output.resize(authCodeLength);
+
+    return (std::equal(output.begin(), output.end(), integrityData));
+}
+
+Buffer AlgoSHA1::generateIntegrityData(const Buffer& packet) const
+{
+    Buffer output(SHA_DIGEST_LENGTH);
+    unsigned int mdLen = 0;
+
+    if (HMAC(EVP_sha1(),
+             key1.data(), key1.size(),
+             packet.data() + message::parser::RMCP_SESSION_HEADER_SIZE,
+             packet.size() - message::parser::RMCP_SESSION_HEADER_SIZE,
+             output.data(), &mdLen) == NULL)
+    {
+        throw std::runtime_error("Generating integrity data for response "
+                                 "failed");
+    }
+
+    return output;
+}
+
 }// namespace integrity
 
 }// namespace cipher
