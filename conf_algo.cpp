24cb339ef0fe2f245e0e20230197bc91ecc16f06
Tom Joseph <tomjoseph@in.ibm.com>
2017-01-25 14:38:49 +0530

    Implementation of AES-CBC-128 Encryption funtion
    Change-Id: I6cf46daa244847e4c9913ede08ced36817ddb724
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- conf_algo.cpp
+++ conf_algo.cpp
@@ -57,7 +57,39 @@ buffer AlgoAES128::decryptPayload(const buffer& packet,
 
 buffer AlgoAES128::encryptPayload(buffer& payload)
 {
+    auto payloadLen = payload.size();
 
+    /*
+     * The following logic calculates the number of padding bytes to be added to
+     * the payload data. This would ensure that the length is a multiple of the
+     * block size of algorithm being used. For the AES algorithm, the block size
+     * is 16 bytes.
+     */
+    auto paddingLen = AESCBC128BlockSize - ((payloadLen + 1) & 0xF);
+
+    /*
+     * The additional field is for the Confidentiality Pad Length field. For the
+     * AES algorithm, this number will range from 0 to 15 bytes. This field is
+     * mandatory.
+     */
+    payload.resize(payloadLen + paddingLen + 1);
+
+    /*
+     * If no Confidentiality Pad bytes are required, the Confidentiality Pad
+     * Length field is set to 00h. If present, the value of the first byte of
+     * Confidentiality Pad shall be one (01h) and all subsequent bytes shall
+     * have a monotonically increasing value (e.g., 02h, 03h, 04h, etc).
+     */
+    if (0 != paddingLen)
+    {
+        std::iota(payload.begin() + payloadLen,
+                  payload.begin() + payloadLen + paddingLen,
+                  1);
+    }
+
+    payload.back() = paddingLen;
+
+    return encryptData(payload.data(), payload.size());
 }
 
 buffer AlgoAES128::decryptData(const uint8_t* iv,
@@ -109,7 +141,62 @@ buffer AlgoAES128::decryptData(const uint8_t* iv,
 
 buffer AlgoAES128::encryptData(const uint8_t* input, const int inputLen) const
 {
+    buffer output(inputLen + AESCBC128BlockSize);
 
+    // Generate the initialization vector
+    if (!RAND_bytes(output.data(), AESCBC128ConfHeader))
+    {
+        throw std::runtime_error("RAND_bytes failed");
+    }
+
+    EVP_CIPHER_CTX ctx;
+
+    // Initializes Cipher context
+    EVP_CIPHER_CTX_init(&ctx);
+
+    /*
+     * EVP_EncryptInit_ex sets up cipher context ctx for encryption with type
+     * AES-CBC-128. ctx must be initialized before calling this function. K2 is
+     * the symmetric key used and iv is the initialization vector used.
+     */
+    if (!EVP_EncryptInit_ex(&ctx, EVP_aes_128_cbc(), NULL, K2.data(),
+                            output.data()))
+    {
+        throw std::runtime_error("EVP_EncryptInit_ex failed for type "
+                                 "AES-CBC-128");
+    }
+
+    /*
+     * EVP_CIPHER_CTX_set_padding() enables or disables padding. If the pad
+     * parameter is zero then no padding is performed. This function always
+     * returns 1.
+     */
+    EVP_CIPHER_CTX_set_padding(&ctx, 0);
+
+    int outputLen = 0;
+
+    /*
+     * If padding is disabled then EVP_EncryptFinal_ex() will not encrypt any
+     * more data and it will return an error if any data remains in a partial
+     * block: that is if the total data length is not a multiple of the block
+     * size. Since we are adding padding bytes and ensures that payload is a
+     * multiple of block size, we are not making the call to
+     * EVP_DecryptFinal_ex()
+     */
+    if (!EVP_EncryptUpdate(&ctx,
+                           output.data() + AESCBC128ConfHeader,
+                           &outputLen,
+                           input, inputLen))
+    {
+        EVP_CIPHER_CTX_cleanup(&ctx);
+        throw std::runtime_error("EVP_EncryptUpdate failed for type "
+                                 "AES-CBC-128");
+    }
+
+    output.resize(AESCBC128ConfHeader + outputLen);
+    EVP_CIPHER_CTX_cleanup(&ctx);
+
+    return output;
 }
 
 }// namespace integrity
