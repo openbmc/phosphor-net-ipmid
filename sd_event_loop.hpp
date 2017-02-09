8b1c2cfd2b54953aa6aa3da6716d0d1b45fccb85
Tom Joseph <tomjoseph@in.ibm.com>
2017-02-09 19:49:38 +0530

    Interfaces for the sd_event_loop adaptation
    The sd_event_loop is adapted for handling the udp socket for IPMI
    network traffic, host console unix socket, character accumulate
    interval timer & retry interval timer for the SOL payload.
    
    Change-Id: I6ccec70821c01ae6db0bf04049a61e7854a798ec
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ sd_event_loop.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+#include <systemd/sd-event.h>
+#include <map>
+
+namespace eventloop
+{
+
+using EventSource = sd_event_source *;
+
+class EventLoop
+{
+    public:
+
+        EventLoop() = default;
+        ~EventLoop() = default;
+        EventLoop(const EventLoop&) = delete;
+        EventLoop& operator=(const EventLoop&) = delete;
+        EventLoop(EventLoop&&) = delete;
+        EventLoop& operator=(EventLoop&&) = delete;
+
+        /** @brief SOL payload instance is the key for the map */
+        using payloadMap = std::map<uint8_t, std::tuple<EventSource, uint64_t,
+              EventSource, uint64_t>>;
+
+        /**
+         * @brief Initialise the event loop and add the handler for incoming
+         *        IPMI packets
+         *
+         * @return EXIT_SUCCESS on success and EXIT_FAILURE on failure.
+         */
+        int startEventLoop();
+
+        /*
+         * @brief Adds Host Console I/O Event source to the event loop
+         *
+         * @return 0 on success and negative error number on error
+         */
+        int startConsolePayload(int fd);
+
+        /**
+         * @brief Destroy Host Console I/O Event source
+         *
+         * @return 0 on success and negative error number on error
+         */
+        int stopConsolePayload();
+
+        /**
+         * @brief Initialize the timers for the SOL Payload Instance
+         *
+         * This API would add the Character accumulate interval timer event
+         * source and the retry interval timer event source for the SOL payload
+         * instance to the event loop.
+         *
+         * @param[in] payloadInst - SOL Payload Instance
+         * @param[in] accumulateInterval - Character Accumulate Interval in
+         *                                 micro seconds
+         * @param[in] retryInterval - Retry interval in microseconds
+         *
+         * @return 0 on success and negative error number on error
+         */
+        int startSOLPayloadInstance(uint8_t payloadInst,
+                                    uint64_t accumulateInterval,
+                                    uint64_t retryInterval);
+
+        /**
+         * @brief Stop the timers for the SOL Payload Instance
+         *
+         * This would remove the Character accumulate interval timer event
+         * source and the retry interval timer event source from the event loop.
+         *
+         * @param[in] payloadInst - SOL Payload Instance
+         *
+         * @return 0 on success and negative error number on error
+         */
+        int stopSOLPayloadInstance(uint8_t payloadInst);
+
+        /**
+         * @brief Modify the Character accumulate timer event source to enable/
+         *        disable
+         *
+         * @param[in] payloadInst- SOL Payload Instance
+         * @param[in] status - on/off the event source
+         */
+        int switchAccumulateTimer(uint8_t payloadInst, bool status);
+
+        /**
+         * @brief Modify the retry interval timer event source to enable/disable
+         *
+         * @param[in] payloadInst- SOL Payload Instance
+         * @param[in] status - on/off the event source
+         */
+        int switchRetryTimer(uint8_t payloadInst, bool status);
+
+    private:
+
+        /** @brief Event Source object for Host Console */
+        EventSource hostConsole = nullptr;
+
+        /**
+         * @brief Event Source for the UDP socket listening on IPMI standard
+         *         port
+         */
+        EventSource udpIPMI = nullptr;
+
+        /**
+         * Map to keep information regarding IPMI Payload instance and timers
+         * for Character accumulate interval and Retry Interval
+         */
+        payloadMap payloadInfo;
+
+        /** Event Loop Object */
+        sd_event* event = nullptr;
+};
+
+} // namespace eventloop
