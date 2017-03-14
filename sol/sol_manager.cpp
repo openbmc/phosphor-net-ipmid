a27364ccfaeb71a35586cba602192f38e1585668
Tom Joseph <tomjoseph@in.ibm.com>
2017-03-14 18:09:11 +0530

    Add API to start a SOL payload instance
    Change-Id: I3c87936350c9e3d7c4eb97b4c720295652aff9c4
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- sol/sol_manager.cpp
+++ sol/sol_manager.cpp
@@ -62,4 +62,48 @@ int Manager::writeConsoleSocket(const sol::buffer& input)
     return 0;
 }
 
+void Manager::startSOLPayload(uint8_t payloadInstance, uint32_t sessionID)
+{
+    int rc = 0;
+
+    if (payloadMap.size() == 0)
+    {
+        rc = initHostConsoleFd();
+
+        if (rc < 0)
+        {
+            throw std::runtime_error("Initialising the host console socket "
+                                     "failed");
+        }
+
+        // Register the fd in the sd_event_loop
+        rc = std::get<eventloop::EventLoop&>(singletonPool).
+                startConsolePayload(fd);
+        if (rc < 0)
+        {
+            throw std::runtime_error("Registering the host console socket "
+                                     "to sd_event_loop failed");
+        }
+    }
+
+    // Create the SOL Context data for payload instance
+    auto context = std::make_unique<Context>(
+            accumulateInterval, retryCount, payloadInstance, sessionID);
+
+    payloadMap.emplace(payloadInstance, std::move(context));
+
+
+    /*
+     * Start payload event instance
+     *
+     * Accumulate interval is in 5 ms(millis sec) increments, since
+     * sd_event_add_time takes in micro secs, it is converted to micro secs.
+     * The Retry interval is in 10 ms (milli secs) increments.
+     */
+    std::get<eventloop::EventLoop&>(singletonPool).startSOLPayloadInstance(
+            payloadInstance,
+            accumulateInterval * 5 * pow(10, 6),
+            retryThreshold * 10 * pow(10, 6));
+}
+
 } // namespace sol
