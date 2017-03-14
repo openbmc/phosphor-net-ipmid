07c6b7f3b118f4966e6aa57b86719649ff10b7c2
Tom Joseph <tomjoseph@in.ibm.com>
2017-03-14 18:04:22 +0530

    Implement Host Console socket Init
    This API would initialise the host console socket file descriptor
    
    Change-Id: Ied7bdc1c4fbee060ec693d8bab3c7bade6edf365
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ sol/sol_manager.cpp
@@ -0,0 +1,41 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <cmath>
+#include <phosphor-logging/log.hpp>
+#include "main.hpp"
+#include "sol_context.hpp"
+#include "sol_manager.hpp"
+
+namespace sol
+{
+
+using namespace phosphor::logging;
+
+void Manager::initHostConsoleFd()
+{
+    struct sockaddr_un addr;
+    int rc = 0;
+
+    fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0)
+    {
+        log<level::ERR>("Failed to open the host console socket",
+                entry("ERRNO=%d", errno));
+        throw std::runtime_error("Failed to open the host console socket");
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    memcpy(&addr.sun_path, &CONSOLE_SOCKET_PATH, CONSOLE_SOCKET_PATH_LEN);
+
+    rc = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (rc < 0)
+    {
+        log<level::ERR>("Failed to connect to host console socket address",
+                entry("ERRNO=%d", errno));
+        throw std::runtime_error("Failed to connect to console server");
+        close(fd);
+    }
+}
+
+} // namespace sol
