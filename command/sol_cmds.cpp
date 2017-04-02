8d75def0a2f8fcea6520122439c1bcfcd59c01aa
Tom Joseph <tomjoseph@in.ibm.com>
2017-04-03 01:56:04 +0530

    Add command handler for SOL activating command.
    Change-Id: I81094330b3fa7f320bd4518cabcfb8fc5b75411c
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- command/sol_cmds.cpp
+++ command/sol_cmds.cpp
@@ -48,6 +48,28 @@ std::vector<uint8_t> payloadHandler(std::vector<uint8_t>& inPayload,
     return response;
 }
 
+void activating(uint8_t payloadInstance, uint32_t sessionID)
+{
+    std::vector<uint8_t> outPayload(sizeof(SOLActivatingRequest));
+
+    auto request = reinterpret_cast<SOLActivatingRequest*>
+                    (outPayload.data());
+
+    request->sessionState = 0;
+    request->payloadInstance = payloadInstance;
+    request->majorVersion = sol::MAJOR_VERSION;
+    request->minorVersion = sol::MINOR_VERSION;
+
+    auto session = (std::get<session::Manager&>(singletonPool).getSession(
+            sessionID)).lock();
+
+    message::Handler msgHandler(sessionID, session->channelPtr);
+
+    msgHandler.sendUnsolicitedIPMIPayload(NETFN_TRANSPORT,
+                                          SOL_ACTIVATING_COMMAND,
+                                          outPayload);
+}
+
 } // command
 
 } // sol
