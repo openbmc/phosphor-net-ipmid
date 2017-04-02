e5d42fa23ff61c4f7c5e7ee280b393d89395afae
Tom Joseph <tomjoseph@in.ibm.com>
2017-04-03 01:53:44 +0530

    Add handler function for incoming SOL payload.
    Change-Id: I1bdff462ef43281332c6b8c2d61028bc0d9f87b0
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ command/sol_cmds.cpp
@@ -0,0 +1,53 @@
+#include <phosphor-logging/log.hpp>
+#include "main.hpp"
+#include "sol/sol_context.hpp"
+#include "sol/sol_manager.hpp"
+#include "sol_cmds.hpp"
+
+namespace sol
+{
+
+namespace command
+{
+
+using namespace phosphor::logging;
+
+std::vector<uint8_t> payloadHandler(std::vector<uint8_t>& inPayload,
+                                    const message::Handler& handler)
+{
+    std::vector<uint8_t> response;
+    auto request = reinterpret_cast<Payload*>(inPayload.data());
+
+    auto solDataSize = inPayload.size() - sizeof(Payload);
+
+    Buffer charData(solDataSize);
+    if( solDataSize > 0)
+    {
+        std::copy_n(inPayload.data() + sizeof(Payload),
+                    solDataSize,
+                    charData.begin());
+    }
+
+    try
+    {
+        auto& context = std::get<sol::Manager&>(singletonPool).
+                getContext(handler.sessionID);
+
+        context.processInboundPayload(request->packetSeqNum,
+                                      request->packetAckSeqNum,
+                                      request->acceptedCharCount,
+                                      request->inOperation.ack,
+                                      charData);
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>(e.what());
+        return response;
+    }
+
+    return response;
+}
+
+} // namespace command
+
+} // namespace sol
