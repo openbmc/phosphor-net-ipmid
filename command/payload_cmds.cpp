c4766bbd24c280c029195ac56b025a8a173cbae5
Tom Joseph <tomjoseph@in.ibm.com>
2017-07-31 18:48:34 +0530

    sol: Handle deactivate payload correctly when session is closed
    Resolves openbmc/openbmc#1781
    Resolves openbmc/openbmc#2053
    
    Change-Id: Iabb61fc510d33a4132e7ad07184eb8847301b686
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- command/payload_cmds.cpp
+++ command/payload_cmds.cpp
@@ -127,13 +127,28 @@ std::vector<uint8_t> deactivatePayload(const std::vector<uint8_t>& inPayload,
                 (request->payloadInstance);
         auto sessionID = context.sessionID;
 
-        activating(request->payloadInstance, sessionID);
         std::get<sol::Manager&>(singletonPool).stopPayloadInstance(
                 request->payloadInstance);
 
+        try
+        {
+            activating(request->payloadInstance, sessionID);
+        }
+        catch (std::exception& e)
+        {
+            log<level::INFO>(e.what());
+            /*
+             * In case session has been closed (like in the case of inactivity
+             * timeout), then activating function would throw an exception,
+             * since sessionID is not found. Since the session is already
+             * closed, we would return with success completion code.
+             */
+            return outPayload;
+        }
+
         auto check = std::get<session::Manager&>(singletonPool).stopSession
                 (sessionID);
-        if(!check)
+        if (!check)
         {
             response->completionCode = IPMI_CC_UNSPECIFIED_ERROR;
         }
