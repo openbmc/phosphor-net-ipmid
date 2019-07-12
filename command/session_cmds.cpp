601587328a3f0a7a314227d4d427c1c7a14bd5c2
Rajashekar Gade Reddy <raja.sekhar.reddy.gade@linux.intel.com>
2019-07-12 17:35:06 +0000

    Updated close session to close any session
    Added support in close session command to close not only the self
    sessions but also other netipmid sessions.
    
    With this support, now any netipmid instance will be able to close
    any other netipmid instance's session.
    
    Tested close session command for both self and other netipmid sessions:
    
    ipmitool .. raw 0x6 0x3c <session id >
     00
    Active session  is closed.
    
    Signed-off-by: Rajashekar Gade Reddy <raja.sekhar.reddy.gade@linux.intel.com>
    Change-Id: Ieade8e342777a940d30248bb23f67dc3c787a001

--- command/session_cmds.cpp
+++ command/session_cmds.cpp
@@ -5,8 +5,12 @@
 
 #include <ipmid/api.h>
 
+#include <ipmid/utils.hpp>
+#include <phosphor-logging/log.hpp>
+
 namespace command
 {
+using namespace phosphor::logging;
 
 std::vector<uint8_t>
     setSessionPrivilegeLevel(const std::vector<uint8_t>& inPayload,
@@ -62,6 +66,152 @@ std::vector<uint8_t>
     return outPayload;
 }
 
+bool parseCloseSessionInputPayload(const std::string& objectPath,
+                                   uint32_t& sessionID, uint8_t& sessionHandle)
+{
+    std::size_t ptrPosition = objectPath.rfind("/");
+    uint16_t tempSessionHandle = 0;
+
+    if (ptrPosition != std::string::npos)
+    {
+        std::string sessionIdString = objectPath.substr(ptrPosition + 1);
+        std::size_t pos = sessionIdString.rfind("_");
+
+        if (pos != std::string::npos)
+        {
+            std::string sessionHandleString = sessionIdString.substr(pos + 1);
+            sessionIdString = sessionIdString.substr(0, pos);
+            std::stringstream handle(sessionHandleString);
+            handle >> std::hex >> tempSessionHandle;
+            sessionHandle = tempSessionHandle & 0xFF;
+            std::stringstream ID(sessionIdString);
+            ID >> std::hex >> sessionID;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isSessionObjectMatched(const std::string& objectPath,
+                            const uint32_t reqSessionID,
+                            const uint8_t reqSessionHandle)
+{
+    uint32_t sessionID = 0;
+    uint8_t sessionHandle = 0;
+
+    if (parseCloseSessionInputPayload(objectPath, sessionID, sessionHandle) ==
+        true)
+    {
+        if ((reqSessionID == session::sessionZero &&
+             reqSessionHandle == sessionHandle) ||
+            (reqSessionID != session::sessionZero && reqSessionID == sessionID))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+uint8_t setSessionState(std::shared_ptr<sdbusplus::asio::connection>& busp,
+                        const std::string& service, const std::string& obj)
+{
+    try
+    {
+        uint8_t sessionState = std::get<uint8_t>(ipmi::getDbusProperty(
+            *busp, service, obj, session::sessionIntf, "State"));
+
+        if (sessionState == static_cast<uint8_t>(session::State::active))
+        {
+            ipmi::setDbusProperty(
+                *busp, service, obj, session::sessionIntf, "State",
+                static_cast<uint8_t>(session::State::tearDownInProgress));
+            return ipmi::ccSuccess;
+        }
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>("Failed in getting session State property",
+                        entry("service=%s", service.c_str()),
+                        entry("Object path=%s", obj.c_str()),
+                        entry("interface=%s", session::sessionIntf));
+        return ipmi::ccUnspecifiedError;
+    }
+
+    return ipmi::ccInvalidFieldRequest;
+}
+
+uint8_t closeOtherNetInstanceSession(const uint32_t reqSessionID,
+                                     const uint8_t reqSessionHandle)
+{
+    auto busp = getSdBus();
+    ipmi::ObjectTree objectTree;
+
+    try
+    {
+        objectTree = ipmi::getAllDbusObjects(
+            *busp, session::sessionManagerRootPath, session::sessionIntf);
+    }
+    catch (sdbusplus::exception::SdBusError& e)
+    {
+        log<level::ERR>("Failed to fetch object from dbus",
+                        entry("INTERFACE=%s", session::sessionIntf),
+                        entry("ERRMSG=%s", e.what()));
+        return ipmi::ccUnspecifiedError;
+    }
+
+    for (auto& objectTreeItr : objectTree)
+    {
+        const std::string obj = objectTreeItr.first;
+
+        if (isSessionObjectMatched(obj, reqSessionID, reqSessionHandle) == true)
+        {
+            auto& serviceMap = objectTreeItr.second;
+
+            if (serviceMap.size() != 1)
+            {
+                return ipmi::ccUnspecifiedError;
+            }
+
+            auto itr = serviceMap.begin();
+            const std::string service = itr->first;
+            return setSessionState(busp, service, obj);
+        }
+    }
+
+    return ipmi::ccInvalidFieldRequest;
+}
+
+uint8_t closeMyNetInstanceSession(uint32_t reqSessionID,
+                                  uint8_t reqSessionHandle)
+{
+    bool status = false;
+
+    try
+    {
+        if (reqSessionID == session::sessionZero)
+        {
+            reqSessionID = std::get<session::Manager&>(singletonPool)
+                               .getSessionIDbyHandle(reqSessionHandle);
+        }
+
+        status = std::get<session::Manager&>(singletonPool)
+                     .stopSession(reqSessionID);
+        if (!status)
+        {
+            return session::ccInvalidSessionId;
+        }
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>("Failed to get session manager instance",
+                        entry("ERRMSG=%s", e.what()));
+        return ipmi::ccUnspecifiedError;
+    }
+
+    return ipmi::ccSuccess;
+}
+
 std::vector<uint8_t> closeSession(const std::vector<uint8_t>& inPayload,
                                   const message::Handler& handler)
 {
@@ -69,25 +219,53 @@ std::vector<uint8_t> closeSession(const std::vector<uint8_t>& inPayload,
     auto request =
         reinterpret_cast<const CloseSessionRequest*>(inPayload.data());
     auto response = reinterpret_cast<CloseSessionResponse*>(outPayload.data());
-    response->completionCode = IPMI_CC_OK;
+    uint32_t reqSessionID = request->sessionID;
+    uint8_t ipmiNetworkInstance = 0;
+    uint8_t reqSessionHandle = request->sessionHandle;
 
-    auto bmcSessionID = endian::from_ipmi(request->sessionID);
-
-    // Session 0 is needed to handle session setup, so session zero is never
-    // closed
-    if (bmcSessionID == session::sessionZero)
+    if (reqSessionID == session::sessionZero &&
+        reqSessionHandle == session::invalidSessionHandle)
     {
-        response->completionCode = IPMI_CC_INVALID_SESSIONID;
+        response->completionCode = session::ccInvalidSessionHandle;
+        return outPayload;
+    }
+
+    if (inPayload.size() == sizeof(reqSessionID) &&
+        reqSessionID == session::sessionZero)
+    {
+        response->completionCode = session::ccInvalidSessionId;
+        return outPayload;
+    }
+
+    try
+    {
+        ipmiNetworkInstance =
+            std::get<session::Manager&>(singletonPool).getNetworkInstance();
+    }
+    catch (sdbusplus::exception::SdBusError& e)
+    {
+        log<level::ERR>("Failed to fetch object from dbus",
+                        entry("INTERFACE=%s", session::sessionIntf),
+                        entry("ERRMSG=%s", e.what()));
+        response->completionCode = ipmi::ccUnspecifiedError;
+        return outPayload;
+    }
+
+    if (reqSessionID >> myNetInstanceSessionIdShiftMask ==
+            ipmiNetworkInstance ||
+        (reqSessionID == session::sessionZero &&
+         (reqSessionHandle >> myNetInstanceSessionHandleShiftMask ==
+          ipmiNetworkInstance)))
+    {
+        response->completionCode =
+            closeMyNetInstanceSession(reqSessionID, reqSessionHandle);
     }
     else
     {
-        auto status = std::get<session::Manager&>(singletonPool)
-                          .stopSession(bmcSessionID);
-        if (!status)
-        {
-            response->completionCode = IPMI_CC_INVALID_SESSIONID;
-        }
+        response->completionCode =
+            closeOtherNetInstanceSession(reqSessionID, reqSessionHandle);
     }
+
     return outPayload;
 }
 
