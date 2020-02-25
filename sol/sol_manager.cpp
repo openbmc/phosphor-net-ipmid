54cfd99bab73d8e16465b42b57a9ce11a9c911ed
Cheng C Yang <cheng.c.yang@linux.intel.com>
2020-02-25 12:05:07 +0800

    Add dbus interface for sol commands
    Add dbus interface for sol config parameters so that after move set/get
    sol config parameter command from net-ipmid to host-ipmid, the command
    can send config parameters to net-ipmid sol service through the dbus
    interface.
    
    Tested by:
    busctl introspect xyz.openbmc_project.Settings /xyz/openbmc_project
    /network/host0/sol can show correct dbus properties of sol parameters.
    ipmitool -I lanplus -H x -U x -P x raw 0x0c 0x21 0x0e 0x00 0x01
    ipmitool -I lanplus -H x -U x -P x raw 0x0c 0x21 0x0e 0x01 0x00
    ipmitool -I lanplus -H x -U x -P x raw 0x0c 0x21 0x0e 0x02 0x83
    ipmitool -I lanplus -H x -U x -P x raw 0x0c 0x21 0x0e 0x03 0x5 0x03
    ipmitool -I lanplus -H x -U x -P x raw 0x0c 0x21 0x0e 0x04 0x5 0x03
    all these commands can change the dbus properties as the value in
    above commands.
    Before and after run these commands, ipmitool -I lanplus -H x -U x
    -P x sol activate can start sol session correctly.
    After reboot BMC, "Progress" property in dbus interface change back
    to 0 and other properties will not reset to default value.
    
    Signed-off-by: Cheng C Yang <cheng.c.yang@linux.intel.com>
    Change-Id: Ib441b551a1559908c427be5378ff3414693e20dd

--- sol/sol_manager.cpp
+++ sol/sol_manager.cpp
@@ -12,7 +12,13 @@
 #include <boost/asio/write.hpp>
 #include <chrono>
 #include <cmath>
+#include <ipmid/utils.hpp>
 #include <phosphor-logging/log.hpp>
+#include <sdbusplus/message/types.hpp>
+
+constexpr const char* solInterface = "xyz.openbmc_project.Ipmi.SOL";
+constexpr const char* solPath = "/xyz/openbmc_project/ipmi/sol/";
+constexpr const char* PROP_INTF = "org.freedesktop.DBus.Properties";
 
 namespace sol
 {
@@ -91,6 +97,125 @@ void Manager::stopHostConsole()
         consoleSocket->cancel();
         consoleSocket.reset();
     }
+}
+
+std::string getService(sdbusplus::bus::bus& bus, const std::string& intf,
+                       const std::string& path)
+{
+    auto mapperCall =
+        bus.new_method_call("xyz.openbmc_project.ObjectMapper",
+                            "/xyz/openbmc_project/object_mapper",
+                            "xyz.openbmc_project.ObjectMapper", "GetObject");
+
+    mapperCall.append(path);
+    mapperCall.append(std::vector<std::string>({intf}));
+
+    std::map<std::string, std::vector<std::string>> mapperResponse;
+
+    try
+    {
+        auto mapperResponseMsg = bus.call(mapperCall);
+        mapperResponseMsg.read(mapperResponse);
+    }
+    catch (sdbusplus::exception_t&)
+    {
+        throw std::runtime_error("ERROR in mapper call");
+    }
+
+    if (mapperResponse.begin() == mapperResponse.end())
+    {
+        throw std::runtime_error("ERROR in reading the mapper response");
+    }
+
+    return mapperResponse.begin()->first;
+}
+
+ipmi::PropertyMap getAllDbusProperties(sdbusplus::bus::bus& bus,
+                                       const std::string& service,
+                                       const std::string& objPath,
+                                       const std::string& interface)
+{
+    ipmi::PropertyMap properties;
+
+    sdbusplus::message::message method = bus.new_method_call(
+        service.c_str(), objPath.c_str(), PROP_INTF, "GetAll");
+
+    method.append(interface);
+
+    try
+    {
+        sdbusplus::message::message reply = bus.call(method);
+        reply.read(properties);
+    }
+    catch (sdbusplus::exception_t&)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Failed to get all properties",
+            phosphor::logging::entry("PATH=%s", objPath.c_str()),
+            phosphor::logging::entry("INTERFACE=%s", interface.c_str()));
+        throw std::runtime_error("ERROR in reading proerties");
+    }
+
+    return properties;
+}
+
+void Manager::updateSOLParameter(uint8_t channelNum)
+{
+    std::variant<uint8_t, bool> value;
+    sdbusplus::bus::bus dbus(ipmid_get_sd_bus_connection());
+    static std::string solService{};
+    ipmi::PropertyMap properties;
+    std::string ethdevice = ipmi::getChannelName(channelNum);
+    std::string solPathWitheEthName = solPath + ethdevice;
+    if (solService.empty())
+    {
+        try
+        {
+            solService = getService(dbus, solInterface, solPathWitheEthName);
+        }
+        catch (const std::runtime_error& e)
+        {
+            solService.clear();
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "Error: get SOL service failed");
+            return;
+        }
+    }
+    try
+    {
+        properties = getAllDbusProperties(dbus, solService, solPathWitheEthName,
+                                          solInterface);
+    }
+    catch (const std::runtime_error&)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Error setting sol parameter");
+        return;
+    }
+
+    progress = std::get<uint8_t>(properties["Progress"]);
+
+    enable = std::get<bool>(properties["Enable"]);
+
+    forceEncrypt = std::get<bool>(properties["ForceEncryption"]);
+
+    forceAuth = std::get<bool>(properties["ForceAuthentication"]);
+
+    solMinPrivilege = static_cast<session::Privilege>(
+        std::get<uint8_t>(properties["Privilege"]));
+
+    accumulateInterval =
+        std::get<uint8_t>((properties["AccumulateIntervalMS"])) *
+        sol::accIntervalFactor * 1ms;
+
+    sendThreshold = std::get<uint8_t>(properties["Threshold"]);
+
+    retryCount = std::get<uint8_t>(properties["RetryCount"]);
+
+    retryInterval = std::get<uint8_t>(properties["RetryIntervalMS"]) *
+                    sol::retryIntervalFactor * 1ms;
+
+    return;
 }
 
 void Manager::startPayloadInstance(uint8_t payloadInstance,
