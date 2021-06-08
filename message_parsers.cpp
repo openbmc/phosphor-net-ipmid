3f4aded713b1f87cdc05d539ed1919a18e0563fb
Vernon Mauery <vernon.mauery@linux.intel.com>
2021-06-08 15:54:41 -0700

    Enforce encryption/authentication on in-session RMCP+ messages
    In-session RMCP+ messages must have the encryption set to match the
    negotiated encryption setting that was set at session creation time.
    
    Tested: Attempt to send an unencrypted payload with a valid session ID.
    
    Change-Id: Iaa20cb9df9b2224bb81e3be5808cab9202e6dbf4
    Signed-off-by: Vernon Mauery <vernon.mauery@linux.intel.com>

--- message_parsers.cpp
+++ message_parsers.cpp
@@ -176,12 +176,21 @@ std::shared_ptr<Message> unflatten(std::vector<uint8_t>& inPacket)
         throw std::runtime_error("IPMI2.0 Session Header Missing");
     }
 
-    auto message = std::make_shared<Message>();
-
     auto header = reinterpret_cast<SessionHeader_t*>(inPacket.data());
 
+    uint32_t sessionID = endian::from_ipmi(header->sessId);
+
+    auto session =
+        std::get<session::Manager&>(singletonPool).getSession(sessionID);
+    if (!session)
+    {
+        throw std::runtime_error("RMCP+ message from unknown session");
+    }
+
+    auto message = std::make_shared<Message>();
+
     message->payloadType = static_cast<PayloadType>(header->payloadType & 0x3F);
-    message->bmcSessionID = endian::from_ipmi(header->sessId);
+    message->bmcSessionID = sessionID;
     message->sessionSeqNum = endian::from_ipmi(header->sessSeqNum);
     message->isPacketEncrypted =
         ((header->payloadType & PAYLOAD_ENCRYPT_MASK) ? true : false);
@@ -189,6 +198,17 @@ std::shared_ptr<Message> unflatten(std::vector<uint8_t>& inPacket)
         ((header->payloadType & PAYLOAD_AUTH_MASK) ? true : false);
     message->rmcpMsgClass =
         static_cast<ClassOfMsg>(header->base.rmcp.classOfMsg);
+
+    bool integrityMismatch =
+        session->isIntegrityAlgoEnabled() && !message->isPacketAuthenticated;
+    bool encryptMismatch =
+        session->isCryptAlgoEnabled() && !message->isPacketEncrypted;
+
+    if (sessionID != session::sessionZero &&
+        (integrityMismatch || encryptMismatch))
+    {
+        throw std::runtime_error("unencrypted or unauthenticated message");
+    }
 
     size_t payloadLen = endian::from_ipmi(header->payloadLength);
     if ((payloadLen == 0) || (payloadLen > (inPacket.size() - sizeof(*header))))
