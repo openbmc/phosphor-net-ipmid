6997b791fb09036e3fe191424e9e7dbbc3d3e647
Patrick Williams <patrick@stwcx.xyz>
2021-09-02 11:30:44 -0500

    command_table: fix duplicate definition of Value
    The ipmid/types.hpp already has a Value definition which is
    effectively the same as the one defined here, except for a few
    additional variant types.  Use that one instead.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ia67c03f14170a43e27d86c46421a6d012f08c5e1

--- command_table.cpp
+++ command_table.cpp
@@ -1,11 +1,11 @@
 #include "command_table.hpp"
-
 #include "main.hpp"
 #include "message_handler.hpp"
 #include "message_parsers.hpp"
 #include "sessions_manager.hpp"
 
 #include <iomanip>
+#include <ipmid/types.hpp>
 #include <main.hpp>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
@@ -14,13 +14,6 @@
 
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 using namespace phosphor::logging;
-
-namespace ipmi
-{
-using Value = std::variant<bool, uint8_t, int16_t, uint16_t, int32_t, uint32_t,
-                           int64_t, uint64_t, double, std::string>;
-
-} // namespace ipmi
 
 namespace command
 {
