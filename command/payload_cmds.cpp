fa4f3c96aa9c15fec38d0b264552c135338fb4f6
Tang Yiwei <tangyiwei.2022@bytedance.com>
2022-05-17 18:21:03 +0800

    Add privilege check in activate payload
    When a user's privilege is lower than sol privilege level, payload activate
    and deactivate command will be executed failed with this user. Refer to
    ipmi spec v2.0 Appendix G, The configuration parameters for a given payload
    type determine the privilege level required to activate / deactivate the
    payload.
    
    Tested:
    1. Check sol privilege level
    ~# ipmitool -I lanplus -H $ip -U $username -P $password sol activate
    [SOL Session operational.  Use ~? for help]
    SOL session closed by BMC
    ~# ipmitool -I lanplus -H $ip -U $username -P $password sol info
    Info: SOL parameter 'Nonvolatile Bitrate (5)' not supported
    Info: SOL parameter 'Volatile Bitrate (6)' not supported
    Set in progress                 : set-complete
    Enabled                         : true
    Force Encryption                : false
    Force Authentication            : false
    Privilege Level                 : ADMINISTRATOR
    Character Accumulate Level (ms) : 60
    Character Send Threshold        : 96
    Retry Count                     : 7
    Retry Interval (ms)             : 500
    Volatile Bit Rate (kbps)        : IPMI-Over-Serial-Setting
    Non-Volatile Bit Rate (kbps)    : IPMI-Over-Serial-Setting
    Payload Channel                 : 1 (0x01)
    Payload Port                    : 623
    2. Set the privilege of user to 'Operator', run 'sol activate'
    and 'sol deactivate'
    ~# ipmitool -I lanplus -H $ip -U $username -P $password sol activate -L OPERATOR
    Error activating SOL payload: Insufficient privilege level
    ~# ipmitool -I lanplus -H $ip -U $username -P $password sol deactivate -L OPERATOR
    Error de-activating SOL payload: Insufficient privilege level
    3. Set the privilege of user to 'ReadOnly', run 'sol activate'
    and 'sol deactivate'
    ~# ipmitool -I lanplus -H $ip -U $username -P $password sol activate -L USER
    Error activating SOL payload: Insufficient privilege level
    ~# ipmitool -I lanplus -H $ip -U $username -P $password sol deactivate -L USER
    Error de-activating SOL payload: Insufficient privilege level
    
    Signed-off-by: Tang Yiwei <tangyiwei.2022@bytedance.com>
    Change-Id: Ie5ca60c9891140614b701392c94245e916cd97ab

--- command/payload_cmds.cpp
+++ command/payload_cmds.cpp
@@ -62,6 +62,13 @@ std::vector<uint8_t> activatePayload(const std::vector<uint8_t>& inPayload,
         return outPayload;
     }
 
+    if (session->currentPrivilege() <
+        static_cast<uint8_t>(sol::Manager::get().solMinPrivilege))
+    {
+        response->completionCode = IPMI_CC_INSUFFICIENT_PRIVILEGE;
+        return outPayload;
+    }
+
     // Is SOL Payload enabled for this user & channel.
     auto userId = ipmi::ipmiUserGetUserId(session->userName);
     ipmi::PayloadAccess payloadAccess = {};
@@ -109,7 +116,7 @@ std::vector<uint8_t> activatePayload(const std::vector<uint8_t>& inPayload,
 
 std::vector<uint8_t>
     deactivatePayload(const std::vector<uint8_t>& inPayload,
-                      std::shared_ptr<message::Handler>& /* handler */)
+                      std::shared_ptr<message::Handler>& handler)
 {
     auto request =
         reinterpret_cast<const DeactivatePayloadRequest*>(inPayload.data());
@@ -145,12 +152,20 @@ std::vector<uint8_t>
         return outPayload;
     }
 
+    auto deactivateSession =
+        session::Manager::get().getSession(handler->sessionID);
+    auto& context = sol::Manager::get().getContext(request->payloadInstance);
+    auto sessionID = context.sessionID;
+    auto activateSession = session::Manager::get().getSession(sessionID);
+    if (deactivateSession->currentPrivilege() <
+        activateSession->currentPrivilege())
+    {
+        response->completionCode = IPMI_CC_INSUFFICIENT_PRIVILEGE;
+        return outPayload;
+    }
+
     try
     {
-        auto& context =
-            sol::Manager::get().getContext(request->payloadInstance);
-        auto sessionID = context.sessionID;
-
         sol::Manager::get().stopPayloadInstance(request->payloadInstance);
 
         try
